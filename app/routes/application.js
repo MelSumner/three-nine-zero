c5f8c7dc650537e7c2608e6a237862dde571a5b4
Melanie Sumner <melaniersumner@gmail.com>
2019-05-01 14:37:52 -0500

    trying out an approach with getviewbounds

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,19 +1,21 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { componentNodes } from '../utils/ember-internals';
+
 
 export default Route.extend({
   router: service('router'),
   init() {
     this._super(...arguments);
     this.router.on('routeDidChange', transition => {
-      if (document.activeElement) {
-        document.activeElement.blur();
+      let focusTarget = componentNodes(this).firstNode;
+      if (document.activeElement && this._inserted) {
+         console.log(focusTarget);
       }
-
       if (transition.to !== null) {
         setTimeout(function() {
-          document.body.querySelector('div').setAttribute("tabindex", "-1");
-          document.body.querySelector('div').focus();
+          document.body.querySelector('main').setAttribute("tabindex", "-1");
+          document.body.querySelector('main').focus();
         }, 0);        
       }
     });
